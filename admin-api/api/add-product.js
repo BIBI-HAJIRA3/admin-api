3e5a3a28afaa86a139707b230735d4260aba4b02
BIBI-HAJIRA3 <bibihajirasanu@gmail.com>
2025-09-24 19:07:07 +0530

    Update add-product.js

--- admin-api/api/add-product.js
+++ admin-api/api/add-product.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://ifopkicdlpdqisixhxob.supabase.co",       // 🔑 replace with your project URL
-  "SUPABASE_SERVICE_KEY"                   // ⚠️ service_role key (keep safe!)
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlmb3BraWNkbHBkcWlzaXhoeG9iIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODQ0Mzc1NiwiZXhwIjoyMDc0MDE5NzU2fQ.Mn5UL14jRYC2Xp_DjnO9gAvfJ0EBEc_AeI3gIrk4juA"                   // ⚠️ service_role key (keep safe!)
 );
 
 const ADMIN_PASSWORD = "yourAdminPass123";  // change to your real admin password
@@ -26,5 +26,6 @@ export default async function handler(req, res) {
   if (error) return res.status(400).json({ error: error.message });
   res.status(200).json({ success: true, data });
 }
+
 
 
